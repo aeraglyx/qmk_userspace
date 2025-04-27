2d618fdc849488c32939fe6cb4dfadc6a3469dc7
aeraglyx <48166821+aeraglyx@users.noreply.github.com>
2025-04-28 01:44:53 +0200

    Fix some stuff

--- keyboards/grooovebob/dasbob/keymaps/aeraglyx/keymap.c
+++ keyboards/grooovebob/dasbob/keymaps/aeraglyx/keymap.c
@@ -55,15 +55,15 @@ combo_t key_combos[] = {
 
 
 const keypos_t PROGMEM hand_swap_config[MATRIX_ROWS][MATRIX_COLS] = {
-    {{4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}},
-    {{4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1}},
-    {{4, 2}, {3, 2}, {2, 2}, {1, 2}, {0, 2}},
-    {{2, 3}, {1, 3}, {0, 3}, {0, 0}, {0, 0}},
-
     {{4, 4}, {3, 4}, {2, 4}, {1, 4}, {0, 4}},
     {{4, 5}, {3, 5}, {2, 5}, {1, 5}, {0, 5}},
     {{4, 6}, {3, 6}, {2, 6}, {1, 6}, {0, 6}},
     {{2, 7}, {1, 7}, {0, 7}, {0, 0}, {0, 0}},
+
+    {{4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}},
+    {{4, 1}, {3, 1}, {2, 1}, {1, 1}, {0, 1}},
+    {{4, 2}, {3, 2}, {2, 2}, {1, 2}, {0, 2}},
+    {{2, 3}, {1, 3}, {0, 3}, {0, 0}, {0, 0}},
 };
 
 
@@ -72,32 +72,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BAS] = LAYOUT_split_3x5_3(
 
         KC_B,               KC_L,               LT(_FUN, KC_D),     KC_W,               KC_X,               KC_K,               KC_P,               KC_U,               KC_O,               KC_Y,
-        LCTL_T(KC_N),       RALT_T(KC_R),       LSFT_T(KC_T),       LCTL_T(KC_S),       KC_G,               KC_F,               RSFT_T(KC_H),       RCTL_T(KC_E),       RALT_T(KC_A),       KC_I,
+        LCTL_T(KC_N),       RALT_T(KC_R),       LSFT_T(KC_T),       LCTL_T(KC_S),       KC_G,               KC_F,               RCTL_T(KC_H),       RSFT_T(KC_E),       RALT_T(KC_A),       KC_I,
         KC_Q,               KC_J,               LT(_FUN, KC_M),     KC_C,               KC_Z,               KC_QUOT,            KC_V,               LT(_FUN, KC_COMM),  KC_DOT,             LSFT(KC_SLSH),
-                                                LT(_NUM, QK_REP),   LT(_NAV, KC_SPC),   SH_T(KC_TAB),       KC_ESC,             LT(_SYM, QK_REP),   KC_ESC
+                                                LT(_NUM, QK_REP),   LT(_NAV, KC_SPC),   SH_T(KC_TAB),       KC_ESC,             LT(_SYM, QK_REP),   QK_AREP
     ),
 
     [_NAV] = LAYOUT_split_3x5_3(
 
-        KC_SLEP,            KC_PSCR,            KC_GRAVE,           XXXXXXX,            QK_REBOOT,          QK_REBOOT,          KC_HOME,            KC_UP,              KC_END,             XXXXXXX,
+        XXXXXXX,            KC_PSCR,            KC_GRAVE,           XXXXXXX,            QK_REBOOT,          QK_BOOT,            KC_HOME,            KC_UP,              KC_END,             XXXXXXX,
         OSM(MOD_LGUI),      OSM(MOD_RALT),      OSM(MOD_LCTL),      OSM(MOD_LSFT),      XXXXXXX,            XXXXXXX,            KC_LEFT,            KC_DOWN,            KC_RIGHT,           XXXXXXX,
-        XXXXXXX,            XXXXXXX,            MS_BTN3,            OSM(MOD_MEH),       XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,
+        XXXXXXX,            XXXXXXX,            MS_BTN3,            OSM(MOD_MEH),       KC_SLEP,            XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,            XXXXXXX,
                                                 XXXXXXX,            XXXXXXX,            XXXXXXX,            KC_ENT,             KC_SPC,             XXXXXXX
     ),
 
     [_SYM] = LAYOUT_split_3x5_3(
 
         XXXXXXX,            KC_LBRC,            KC_RBRC,            LSFT(KC_4),         XXXXXXX,            XXXXXXX,            LSFT(KC_5),         LSFT(KC_BSLS),      LSFT(KC_2),         XXXXXXX,
-        LSFT(KC_1),         LCTL(KC_9),         LCTL(KC_0),         KC_EQL,             KC_SCLN,            LSFT(KC_2),         LSFT_T(KC_EQL),     KC_MINS,            LSFT(KC_SCLN),      KC_BSLS,
+        LSFT(KC_1),         LSFT(KC_9),         LSFT(KC_0),         KC_EQL,             KC_SCLN,            LSFT(KC_2),         LSFT_T(KC_EQL),     KC_MINS,            LSFT(KC_SCLN),      KC_BSLS,
         KC_GRV,             LSFT(KC_LBRC),      LSFT(KC_RBRC),      LSFT(KC_3),         XXXXXXX,            LSFT(KC_6),         LSFT(KC_8),         KC_SLSH,            LSFT(KC_GRV),       XXXXXXX,
                                                 XXXXXXX,            LSFT(KC_MINS),      KC_GRV,             KC_LEFT,            XXXXXXX,            KC_RIGHT
     ),
 
     [_NUM] = LAYOUT_split_3x5_3(
 
-        KC_ESC,             KC_UP,              XXXXXXX,            XXXXXXX,            CK_TOGG,            KC_KP_SLASH,        KC_KP_7,            KC_KP_8,            KC_KP_9,            KC_KP_PLUS,
-        KC_LEFT,            RALT_T(KC_DOWN),    LCTL_T(KC_RGHT),    LSFT_T(XXXXXXX),    KC_LGUI,            KC_KP_ASTERISK,     KC_KP_4,            KC_KP_5,            KC_KP_6,            KC_KP_DOT,
-        KC_MPRV,            KC_MSTP,            KC_MPLY,            KC_MNXT,            KC_PSCR,            XXXXXXX,            KC_KP_1,            KC_KP_2,            KC_KP_3,            KC_KP_MINUS,
+        XXXXXXX,            KC_9,               KC_8,               KC_7,               XXXXXXX,            KC_KP_SLASH,        KC_KP_7,            KC_KP_8,            KC_KP_9,            KC_KP_PLUS,
+        KC_0,               KC_6,               KC_5,               KC_4,               XXXXXXX,            KC_KP_ASTERISK,     KC_KP_4,            KC_KP_5,            KC_KP_6,            KC_KP_DOT,
+        XXXXXXX,            KC_3,               KC_2,               KC_1,               XXXXXXX,            XXXXXXX,            KC_KP_1,            KC_KP_2,            KC_KP_3,            KC_KP_MINUS,
                                                 XXXXXXX,            XXXXXXX,            XXXXXXX,            KC_DOWN,            KC_KP_0,            KC_UP
     ),
 
@@ -112,7 +112,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_REF] = LAYOUT_split_3x5_3(
 
         KC_B,               KC_L,               KC_D,               KC_W,               KC_X,               KC_K,               KC_P,               KC_U,               KC_O,               KC_Y,
-        KC_N,               KC_R,               KC_T,               KC_S,               KC_G,               KC_F,               KC_H,               KC_K,               KC_L,               KC_I,
+        KC_N,               KC_R,               KC_T,               KC_S,               KC_G,               KC_F,               KC_H,               KC_E,               KC_A,               KC_I,
         KC_Q,               KC_J,               KC_M,               KC_C,               KC_Z,               KC_QUOT,            KC_V,               KC_COMM,            KC_DOT,             KC_SLSH,
                                                 QK_REP,             KC_SPC,             KC_TAB,             KC_ESC,             QK_REP,             KC_ESC
     ),
